7f86af9592b4e0ef087a08ccb60dd1c8d2e0d57f
laura <laurn.den@yahoo.com>
2021-08-17 15:07:53 +0200

    homework updated

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -1,72 +1,96 @@
-let jumbotron = document.querySelector(".jumbotron");
-let donateBike = document.querySelector(".buttons a:first-child");
-let volunteer = document.querySelector(".buttons a:nth-child(2)");
-let blueBtn = document.querySelector("#blueBtn");
-let orangeBtn = document.querySelector("#orangeBtn");
-let greenBtn = document.querySelector("#greenBtn");
-blueBtn.addEventListener("click", () => changeColor("#588fbd", "#ffa500", "black", "white"));
-orangeBtn.addEventListener("click", () => changeColor("#f0ad4e", "#5751fd", "31b0d5", "white"));
-greenBtn.addEventListener("click", () => changeColor("#87ca8a", "#5751fd", "8c9c08", "white"));
+/* PART 1 */
+const jumbotron = document.querySelector('.jumbotron');
+const donateABike = document.querySelector('#donate-btn');
+const volunteer = document.querySelector('#volunteer-btn');
+// BLUE
+const blueBtn = document.querySelector('#blueBtn');
+blueBtn.addEventListener('click', changeColoursBlue);
 
-
-function changeColor(jumbColor, donateColor, volunteerBack, volunteerText) {
-    jumbotron.style.backgroundColor = jumbColor;
-    donateBike.style.backgroundColor = donateColor;
-    volunteer.style.backgroundColor = volunteerBack;
-    volunteer.style.color = volunteerText;
+function changeColoursBlue() {
+    jumbotron.style.backgroundColor = `#588fbd`;
+    donateABike.style.backgroundColor = `#ffa500`;
+    volunteer.style.backgroundColor = 'black';
+    volunteer.style.color = 'white';
 }
 
+//ORANGE
+const orangeBtn = document.querySelector('#orangeBtn');
+orangeBtn.addEventListener('click', changeColoursOrange);
 
-let form = document.querySelector("form");
-form.addEventListener("submit", checkUserInput);
-let userEmail = document.querySelector("#exampleInputEmail1");
-let exampleText = document.querySelector("#example-text-input");
-let textArea = document.querySelector("#exampleTextarea");
-let inputs = document.querySelectorAll("input");
-let inputsArray = Array.from(inputs);
-
-userEmail.addEventListener("change", changeBackground);
-exampleText.addEventListener("change", changeBackground);
-textArea.addEventListener("change", changeBackground);
-
-function changeBackground(event) {
-    event.target.classList.remove("redBackground");
-    event.target.classList.add("whiteBackground");
-
+function changeColoursOrange() {
+    jumbotron.style.backgroundColor = `#f0ad4e`;
+    donateABike.style.backgroundColor = `#5751fd`;
+    volunteer.style.backgroundColor = `#31b0d5`;
+    volunteer.style.color = 'white';
 }
 
-function checkUserInput(event) {
+//GREEN 
+ const greenBtn = document.querySelector('#greenBtn');
+ greenBtn.addEventListener('click', changeColoursGreen);
+
+ function changeColoursGreen() {
+    jumbotron.style.backgroundColor = `#87ca8a`;
+    donateABike.style.backgroundColor = 'black';
+    volunteer.style.backgroundColor = `#8c9c08`;
+}
+
+/* PART 2 */
+document.querySelector('form').addEventListener('submit', validateForm);
+const inputEmail = document.querySelector('#exampleInputEmail1');
+const inputYourName = document.querySelector('#example-text-input');
+const inputDescribeYourself = document.querySelector('#exampleTextArea');
+
+function validateForm(){
     event.preventDefault();
-    let isEmailValid = userEmail.value.trim().includes("@");
-    let isExampleTextValid = exampleText.value.trim().length > 0;
-    let isTextAreaValid = textArea.value.trim().length > 0;
-    if (isEmailValid && isExampleTextValid && isTextAreaValid) {
-        userEmail.value = "";
-        exampleText.value = "";
-        textArea.value = "";
-
-        userEmail.classList.remove("redBackground");
-        userEmail.classList.add("whiteBackground");
-        exampleText.classList.remove("redBackground");
-        exampleText.classList.add("whiteBackground");
-        textArea.classList.remove("redBackground");
-        textArea.classList.add("whiteBackground");
-
-        alert("Thank you for filling out the form");
+    removeRed();
+    if (inputEmail.value.length > 0 &&
+        inputEmail.value.includes('@') &&
+        inputYourName.value.length > 0 &&
+        inputDescribeYourself.value.length > 0) {
+        alert('Thank you for filling out the form :)');
+        blankOut();
     } else {
-
-        if (!isEmailValid) {
-            userEmail.classList.remove("whiteBackground");
-
-            userEmail.classList.add("redBackground");
-        }
-        if (!isExampleTextValid) {
-            exampleText.classList.remove("whiteBackground");
-            exampleText.classList.add("redBackground");
-        }
-        if (!isTextAreaValid) {
-            textArea.classList.remove("whiteBackground");
-            textArea.classList.add("redBackground");
-        }
+        redEmail();
+        redYourName();
+        redDescribeYourself();        
     }
+}
+
+function redEmail(){
+    if (inputEmail.value.length <= 0) {
+        turnRed(inputEmail);
+        
+    }
+};
+
+function redYourName(){
+    if (inputYourName.value.length <= 0) {
+        turnRed(inputYourName);
+        
+    }
+};
+
+function redDescribeYourself(){
+    if (inputDescribeYourself.value.length <= 0) {
+        turnRed(inputDescribeYourself);
+        
+    }
+};
+
+
+function removeRed() {
+    inputEmail.classList.remove('bg-danger');
+    inputYourName.classList.remove('bg-danger');
+    inputDescribeYourself.classList.remove('bg-danger');
+
+}
+
+function blankOut() {
+    inputEmail.value = '';
+    inputYourName.value = '';
+    inputDescribeYourself.value = '';
+}
+
+function turnRed(input) {
+    input.classList.add('bg-danger');
 }
