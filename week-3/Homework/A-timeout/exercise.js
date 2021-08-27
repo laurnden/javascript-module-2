94630cb4223efee7711a2dc67cbb8883f8eb0731
laura <laurn.den@yahoo.com>
2021-08-27 13:46:39 +0200

    updated homewowork

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -9,3 +9,16 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+setInterval(changeBackgroundColor, 5000);
+
+let backgroundColorOptions = ['pink', 'blue', 'grey', 'yellow', 'orange', 'red'];
+let index = 0;
+
+function changeBackgroundColor (){
+    if (index > backgroundColorOptions.length - 1) {
+        index = 0;
+    }
+    document.body.style.backgroundColor = backgroundColorOptions[index];
+    index++;
+}
--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,5 +1,37 @@
-function setAlarm() {}
+function setAlarm() {
+  let inputedNumbers = document.getElementById('alarmSet');
+  let timeRemaining = document.getElementById('timeRemaining');
+  let initialTime = inputedNumbers.value;
+  inputedNumbers.value = "";
 
+  function setAlarmInternal() {
+    timeRemaining.textContent = 'Time Remaining: ' + timeOutputFnc(initialTime);
+    initialTime--;
+    if (initialTime <= -1) {
+      clearInterval(timer);
+      playAlarm();
+    }
+  }
+
+  function timeOutputFnc(time) {
+    if (time>=60) {
+      var timeParsed= parseInt(time, 10); 
+       var minutes = Math.floor(timeParsed / 60);
+       var seconds = timeParsed - (minutes * 60);
+        return minutes + ":" + seconds;
+       }
+    else if (time<=59&&time>=10) { return "00:" + time }
+    else {return "00:0" + time}
+  }
+ 
+  
+  let timer = setInterval(setAlarmInternal, 1000);  
+
+  
+}
+  
+
+  
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
